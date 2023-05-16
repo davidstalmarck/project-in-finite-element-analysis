d3e5060dda8fe851c35abe7e05d5bdc7519e00fb
David Stålmarck <david.stalmarck@gmail.com>
2023-05-16 17:12:11 +0200

    solew!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import calfem.utils as cfu
 import logging
 
 L = 0.005 # standard length
+h = 1e5
 isolated = 1
 heated = 2
 copper = 3
@@ -15,17 +16,11 @@ nylon = 4
 thickness = [5e-3] # bunda
 Dcopper = np.identity(2)*385 # Constitutive matrix
 Dnylon = np.identity(2)*0.26 ## Constitutive matrix
-l = 5.0     # length
-h = 1.0     # height
-t = 0.2     # thickness
-v = 0.35    # Poisson
-E = 2.1e9   # Young modulus
-ptype = 1   # plane stress
-
 
 
 def createGeometry():
     """STEP 1 - Creating geometry"""
+    # Creating the geometry of our 
     g = cfg.Geometry()
     g.point([0, 0.3*L]) # point 0
     g.point([0, 0.4*L]) # point 1
@@ -49,7 +44,7 @@ def createGeometry():
     g.point([0, 0])
     N = len(g.points)
     for i in range(0, N-2):
-        if i == 2:
+        if i == 1:
             g.spline([i, (i+1)%N], marker = heated) # line 0 # marker = 1e5
         else:
             g.spline([i, (i+1)%N], marker = isolated) # line 0
@@ -60,7 +55,6 @@ def createGeometry():
     g.surface(list(range(19)), marker = copper, )
     g.surface(list(range(13, 21)), marker = nylon) 
     return g
-
 
 def createMesh(g):
     """STEP 2 - Creating mesh"""
@@ -74,47 +68,61 @@ def createMesh(g):
 
     """STEP 3- Show mesh"""
     cfv.figure()
-    cfv.drawMesh(coords=coords,edof=edof,dofs_per_node=mesh.dofsPerNode,el_type=mesh.elType,filled=True)
-    cfv.showAndWait()
+    #cfv.drawMesh(coords=coords,edof=edof,dofs_per_node=mesh.dofsPerNode,el_type=mesh.elType,filled=True)
+    #cfv.showAndWait()
     return coords, edof, dofs, bdofs, elementmarkers
 
+def creatingStiffnessMatrix():
+    g = createGeometry()
+    #cfv.draw_geometry(s1)
+    #¨cfv.draw_geometry(g)
+    cfv.showAndWait()
 
-g = createGeometry()
-
-#cfv.draw_geometry(s1)
-#¨cfv.draw_geometry(g)
-cfv.showAndWait()
-
-coords, edof, dofs, bdofs, elementmarkers = createMesh(g)
-
-
-# Implementing a CALFEM solver
-nDofs = np.size(dofs)
-ex, ey = cfc.coordxtr(edof, coords, dofs) 
-K = np.zeros([nDofs,nDofs]) # global stifness matrix
+    coords, edof, dofs, bdofs, elementmarkers = createMesh(g)
 
 
-
-for eltopo, elx, ely, mark in zip(edof, ex, ey, elementmarkers):
-    if mark == copper:
-        D = Dcopper
-    elif mark == nylon:
-        D = Dnylon
-    else:
-        logging.warning("Potential error, no material found")
-
-    
-    Ke = cfc.flw2te(elx, ely, ep= thickness, D=Dcopper, eq=None)
-    cfc.assem(eltopo, K, Ke)
-
-# boundary conditions
-bc = np.array([],'i')
-bcVal = np.array([],'f')
+    # Implementing a CALFEM solver
+    nDofs = np.size(dofs)
+    ex, ey = cfc.coordxtr(edof, coords, dofs) 
+    K = np.zeros([nDofs,nDofs]) # global stifness matrix
 
 
-fl = 0
-fb = np.zeros(nDofs)
 
-bc, bcVal = cfu.applybc(bdofs, bc, bcVal, left_support, 0.0, 0)
-bc, bcVal = cfu.applybc(bdofs, bc, bcVal, right_support, 0.0, 2)
+    for eltopo, elx, ely, mark in zip(edof, ex, ey, elementmarkers):
+        if mark == copper:
+            D = Dcopper
+        elif mark == nylon:
+            D = Dnylon
+        else:
+            logging.warning("Potential error, no material found")
+
+        
+        Ke = cfc.flw2te(elx, ely, ep= thickness, D=D, eq=None)
+        cfc.assem(eltopo, K, Ke)
+
+    # boundary conditions
+    bc = np.array([],'i')
+    bcVal = np.array([],'f')
+
+
+    fl = np.zeros((nDofs ,1))
+    fb = np.zeros((nDofs,1))
+    f  = fl+fb
+
+    bc, bcVal = cfu.applybc(bdofs, bc, bcVal, isolated, 0.0, 0)
+    bc, bcVal = cfu.applybc(bdofs, bc, bcVal, heated, h)
+    return K, f, bc, bcVal, coords, edof
+
+
+
+K, f, bc, bcVal, coords, edof = creatingStiffnessMatrix()
+
+a, _ = cfc.solveq(K, f,bc, bcVal)
+
+cfv.figure(fig_size=(10,10))
+cfv.draw_nodal_values_shaded(a, coords, edof, title="Temperature")
+cfv.colorbar()
+cfv.show_and_wait()
+
+
 
